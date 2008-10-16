198ce2a552d6ceb18656d2f6092a4ad8eff83f99
chemikhazi <chemikhazi@76cd09a0-80c1-11dd-8d42-05603b9c63e9>
2008-10-16 08:09:41 +0000

    Proofreading of 0.py finished.

--- trunk/0.py
+++ trunk/0.py
@@ -38,45 +38,59 @@ stageCircle = TextureStage("Circle")
 stageCircle.setSort(2)
 
 """
-Please open the model with a text editor. Egg files are human readable. We need
-this information later to understand how the vertex shader and the fragment
-shader work. Some insights:
+Please open the file cube.egg with a text editor. Egg files are human
+readable. We will need this information later on to understand how the
+vertex shader and the fragment shader works. You can also see how the
+model looks like in panda by using the pview model viewer. 
 
-The cube has six faces. Each face has four different vertices. Therefore the
-cube has 24 vertices. Theoretically a cube only need eight vertices. In this
-example each vertex has to be shared by three faces. Problem is, that each
-vertex can only have one color, but what happens if we want that each of the six
-faces has an another color? This is impossible if the cube is only defined with
-eight vertices. There are more disadvantages if we only define the cube with
-eight vertices, we talk later about. The only advantage with less vertices is
-that we have to send less vertices to the graphic card, but in almost all
-applications vertices are not a limiting factor. The memory consumption of
-vertices in contrast to the memory consumption of textures is negligible. As
-already written each vertex has one associated color. Besides the color there is
-one UV coordinate for every vertex.
+The cube has six faces. Each face has four different vertices. Therefore
+this cube has 24 vertices. Theoretically a cube only needs eight
+vertices with each vertex being shared by three faces. The problem with 
+this is that each vertex can only have one color, but what happens if
+we want each of the six faces to be a different color? This is impossible
+if the cube is only defined with eight vertices. There are more 
+disadvantages if we only define the cube with eight vertices, which we 
+will talk about later on. The only advantage of having less vertices is
+that we have to send less vertices to the graphic card but in almost all 
+applications vertices are not a limiting factor. The memory consumption
+of vertices in comparison to the memory consumption of textures is
+negligible. Besides the color entry for a vertex, a vertex also has one 
+UV entry associated with it. 
 
 It may be useful if you modify the vertices manually and see the results.
+
+// A vertex entry in an egg file
+...
+<Vertex> 0 {
+  1.0 1.0 -1.0 // The vertex position
+  <UV> { 1.0 1.0 } // The vertex UV
+  <RGBA> { 1.0 0.0 0.0 1.0 } // The vertex color in RGBA format
+}
+...
+
 """
 
 modelCube = loader.loadModel("cube.egg")
 
 """
-One more thing about colors. When you look at the colors, defined inside the egg
-file you may see that there are eight different colors defined. Black, white,
-red, green, blue, yellow, purple and cyan. Each color is repeated three times.
-When you run this sample you may ask yourself, why does this cube has thousands
-of colors then? Who creates this nice gradients along the edges? We use a new
-word here: Linear Interpolation. Today graphic cards are very good at linear
-interpolation. They can do billions of linear interpolations per second. The
-downside is that sometimes the graphic card can ONLY do linear interpolation and
-you have no choice to change that, even with a shader. Back to the colors. If
-you have a red color (1.0, 0.0, 0.0) on one vertex and a dark blue color (0.0,
-0.0, 0.5) on the other vertex the graphic card simply interpolates the color for
-every pixel between this two vertices, even without shaders (only if requested,
-but Panda3D ask the graphic card to do this). For this consideration the graphic
-card does not know that a color consists of a R(ed), G(reen) and B(lue) part.
-The graphic card interpolates each part of a color for itself. Here is an
-example what a graphic card does:
+Next look at the colors defined in the egg file. If you compare all the 
+color entries, you will only find eight unique colors in the egg file. 
+Why does the model have thousands of colors when viewed in the model 
+viewer then? This is because of linear interpolation, where a value is 
+generated between two different values based on a "distance". Today 
+graphic cards are very good at linear interpolation with the ability to 
+do billions of linear interpolations per second. The downside is that 
+sometimes the graphic card can ONLY do linear interpolation and you can't 
+change that, even with a shader.
+
+Back to the colors. If you have a red color (1.0, 0.0, 0.0) on one vertex 
+and a dark blue color (0.0, 0.0, 0.5) on the other vertex the graphic 
+card simply interpolates the color for every pixel between this two 
+vertices, even without shaders (only if requested, but Panda3D ask the 
+graphic card to do this). The graphic card doesn't know that a color 
+comes in three parts: Red, Green and Blue. It only knows that it is 
+manipulating values, in this case adjusting the constituent values for 
+Red Green and Blue. Here is an example of how the graphic card interpolates: 
 
 100% at red vertex, 0% at dark blue vertex => 1.0, 0.0, 0.0
 75% at red vertex, 25% at dark blue vertex => 0.75, 0.0, 0.125
@@ -84,31 +98,34 @@ example what a graphic card does:
 25% at red vertex, 75% at dark blue vertex => 0.25, 0.0, 0.375
 0% at red vertex, 100% at dark blue vertex => 0.0, 0.0, 0.5
 
-A bit oversimplified (in reality it does not work exactly like this but the
-result is the same): If the graphic card likes to draw a pixel on a screen it
-first looks if this pixel is on a vertex. If yes it can directly take this color
-and draw a pixel with this color. If it is not, the graphic card looks to which
-triangle this pixel belongs. Then it looks were the vertices of this triangle
-are and calculates the distance to each of this vertices. Based on this distance
-and the color of this vertices it interpolates all color components and draws a
-pixel with this color.
+A simplified version of how the graphic card draws the model (in reality 
+it does not work exactly like this but the result is the same): If the 
+graphic card needs to draw a pixel on a screen it first looks if this 
+pixel is on a vertex. If it is, it can take the color of the vertex and 
+draw a pixel with this color. If not, the graphic card looks at which 
+triangle this pixel belongs. Then it looks at where the vertices of this 
+triangle are and calculates the distance to each of the vertices. Based 
+on this distance and the color of the vertices, it interpolates all color 
+components and draws a pixel with this color.
 
-We already said, that the graphic card does not care about the fact that a color
-consists of the three parts R, G and B. The good thing about this is that the
-graphic card can do the calculations for R independent of the other parts. This
-is true for G an B. You may ask: "what should I care"? The advantage is that the
-graphic card can do this in parallel. A graphic card is in general extremely
-specialized in parallel computing. This is also true for vertex shaders and
-pixel shaders. Each calculation for a vertex or pixel is done individually. A
-vertex never knows how his neighbor looks like and a pixel never knows what his
-neighbors color is. This is a reason why graphic card vendors can improve the
-performance of GPUs faster then CPUs. Vertex and pixel shaders are inherently
-parallel. The disadvantage of this is that if anyone likes to do some
-calculations with respect to the neighborhood he has to create a complex setup
-that often (but not always) is not fast enough for 60+ FPS games.
+We've already seen that the graphic card does not care about the fact 
+that a color consists of the three parts R, G and B. The good thing 
+about this is that the graphic card can do the calculations for R 
+independent of the other parts, as is the case for G and B. You may ask, 
+"why should I care"? The advantage is that the graphic card can do 
+calculations for each part in parallel. A graphic card is in general 
+extremely specialized in parallel computing. This is also true for vertex 
+shaders and pixel shaders. Each calculation for a vertex or pixel is done 
+individually. A vertex never knows how what his neighbor looks like and 
+a pixel never knows what his neighbor's color is. This is a reason why 
+graphic card vendors can improve the performance of GPUs faster then CPUs. 
+Vertex and pixel shaders are inherently parallel. The disadvantage of 
+this is that if you need to do some calculations with respect to the 
+neighboring pixel or vertex, you have to create a complex setup that often 
+(but not always) is not fast enough for 60+ FPS games.
 
-A blur (like in the glow example) filter is an example of such a setup. You need
-at least two passes to create such an effect.
+A blur filter (like in the glow example) is an example of such a setup. 
+You need at least two passes to create such an effect. 
 """
 
 cubes = []
